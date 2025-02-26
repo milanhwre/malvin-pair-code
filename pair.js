e9bf699aa2d11634e8f3ba78c1738a95df157e35
Sahil Ansari <138162151+S4H1L9@users.noreply.github.com>
2025-02-26 16:13:02 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -28,7 +28,7 @@ function randomMegaId(length = 6, numberLength = 4) {
 async function uploadCredsToMega(credsPath) {
     try {
         const storage = await new Storage({
-            email: 'anshrajputxd@gmail.com, // Your Mega A/c Email Here
+            email: 'anshrajputxd@gmail.com', // Your Mega A/c Email Here
             password: 'SAHIL09@@' // Your Mega A/c Password Here
         }).ready;
         console.log('Mega storage initialized.');
